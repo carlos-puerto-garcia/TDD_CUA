13cfac154c1c5c0fcb2cc6806cade7ee20c21891
Thanatos613 <alvarezlarry0425@americana.edu.co>
2019-10-30 10:29:43 -0500

    Larry Alvarez Rodriguez

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
 exports.addTest = function(value1, value2) {
     return  value1 + value2;
   }
+  
+module.exports = {
+    vectores: ()=>{
+        return vec = [1,2,3,4,5];
+    }
+}
--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -3,11 +3,12 @@ var assert = chai.assert; // importamos la libreria assert del framework chai
 var should = chai.should();// importamos la libreria should del framework chai
 var expect = chai.expect;// importamos la libreria expect del framework chai
 var calculator = require('../calculator');// llamamos el archivo calculator para uso en las funciones
+const vectores = require('../calculator').vectores;
 
 //JHON DARWIN ACEVEDO
 describe('Prueba con la funcion assert : ', ()=> { // este seria una forma mas TDD
-  describe('Comprovamos la funcion addTest', ()=>{
-    it('Comprovamos el valor retornado usando : assert.equal(value, value): ', ()=> {
+  describe('Comprobamos la funcion addTest', ()=>{
+    it('Comprobamos el valor retornado usando : assert.equal(value, value): ', ()=> {
        result = calculator.addTest(1,1);//colocamos los valores como parametros para la suma
        assert.equal(result, 2);//y seteamos el resultado que da la suma de los dos parametros, con el resultado real
     });
@@ -15,8 +16,8 @@ describe('Prueba con la funcion assert : ', ()=> { // este seria una forma mas T
 })
 //JHON DARWIN ACEVEDO
 describe('Prueba con la funcion should: ', ()=> {// este seria una forma mas VDD
-    describe('Comprovamos la funcion  addTest', ()=>{
-      it('Comprovamos el valor retornado usando : result.should.be.equal(value): ', ()=> {
+    describe('Comprobamos la funcion  addTest', ()=>{
+      it('Comprobamos el valor retornado usando : result.should.be.equal(value): ', ()=> {
          result = calculator.addTest(1,1);//colocamos los valores como parametros para la suma
          result.should.be.equal(2);//y seteamos el resultado que da la suma de los dos parametros, con el resultado real
       })
@@ -24,11 +25,24 @@ describe('Prueba con la funcion should: ', ()=> {// este seria una forma mas VDD
   })
 //JHON DARWIN ACEVEDO
   describe('Prueba con la funcion expect: ', ()=> {// este seria una forma mas VDD
-    describe('Comprovamos la funcion de addTest', ()=>{
-      it('Comprovamos el valor retornado usando: expect(result).to.be.a(value);: ', ()=> {
+    describe('Comprobamos la funcion de addTest', ()=>{
+      it('Comprobamos el valor retornado usando: expect(result).to.be.a(value);: ', ()=> {
          result = calculator.addTest(1,1); //colocamos los valores como parametros para la suma
           expect(result).to.equal(2);//y seteamos el resultado que da la suma de los dos parametros, con el resultado real
       })
     })
   })
   //basicamente los 3 ejemplos hacen lo mismo los diferencia es el metodo utilizado
+
+  //LARRY ALVAREZ RODRIGUEZ
+  describe('Prueba 1:', ()=>{
+      it('Debe verificar que el tamaño del vector sea el correcto', ()=>{
+          assert.lengthOf(vectores(), 5)
+      })
+  })
+
+  describe('Prueba 2:', ()=>{
+      it('Debe verificar que el tipo de dato de la variable es el correcto', ()=>{
+          assert.typeOf(vectores(), 'array')
+      })
+  })
