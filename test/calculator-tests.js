b99d0b4cce95dfb159c7c65ccbc19daf4a4c901f
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2019-10-30 09:41:49 -0500

    Update calculator-tests.js

--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -4,6 +4,7 @@ var should = chai.should();// importamos la libreria should del framework chai
 var expect = chai.expect;// importamos la libreria expect del framework chai
 var calculator = require('../calculator');// llamamos el archivo calculator para uso en las funciones
 
+//JHON DARWIN ACEVEDO
 describe('Prueba con la funcion assert : ', ()=> { // este seria una forma mas TDD
   describe('Comprovamos la funcion addTest', ()=>{
     it('Comprovamos el valor retornado usando : assert.equal(value, value): ', ()=> {
@@ -12,7 +13,7 @@ describe('Prueba con la funcion assert : ', ()=> { // este seria una forma mas T
     });
   });
 })
-
+//JHON DARWIN ACEVEDO
 describe('Prueba con la funcion should: ', ()=> {// este seria una forma mas VDD
     describe('Comprovamos la funcion  addTest', ()=>{
       it('Comprovamos el valor retornado usando : result.should.be.equal(value): ', ()=> {
@@ -21,12 +22,12 @@ describe('Prueba con la funcion should: ', ()=> {// este seria una forma mas VDD
       })
     })
   })
-
+//JHON DARWIN ACEVEDO
   describe('Prueba con la funcion expect: ', ()=> {// este seria una forma mas VDD
     describe('Comprovamos la funcion de addTest', ()=>{
       it('Comprovamos el valor retornado usando: expect(result).to.be.a(value);: ', ()=> {
          result = calculator.addTest(1,1); //colocamos los valores como parametros para la suma
-          expect(result).to.equal(3);//y seteamos el resultado que da la suma de los dos parametros, con el resultado real
+          expect(result).to.equal(2);//y seteamos el resultado que da la suma de los dos parametros, con el resultado real
       })
     })
   })
