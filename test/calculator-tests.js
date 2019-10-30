99daed3f58ccc1a28469cbe3317490fa192a5b86
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2019-10-30 09:29:20 -0500

    Update calculator-tests.js

--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -5,26 +5,26 @@ var expect = chai.expect;// importamos la libreria expect del framework chai
 var calculator = require('../calculator');// llamamos el archivo calculator para uso en las funciones
 
 describe('Prueba con la funcion assert : ', ()=> { // este seria una forma mas TDD
-  describe('Comprovamos la funcion addTest', function(){
-    it('Comprovamos el valor retornado usando : assert.equal(value, value): ', function() {
+  describe('Comprovamos la funcion addTest', ()=>{
+    it('Comprovamos el valor retornado usando : assert.equal(value, value): ', ()=> {
        result = calculator.addTest(1,1);//colocamos los valores como parametros para la suma
        assert.equal(result, 2);//y seteamos el resultado que da la suma de los dos parametros, con el resultado real
     });
   });
 })
 
-describe('Prueba con la funcion should: ', function() {// este seria una forma mas VDD
-    describe('Comprovamos la funcion  addTest', function(){
-      it('Comprovamos el valor retornado usando : result.should.be.equal(value): ', function() {
+describe('Prueba con la funcion should: ', ()=> {// este seria una forma mas VDD
+    describe('Comprovamos la funcion  addTest', ()=>{
+      it('Comprovamos el valor retornado usando : result.should.be.equal(value): ', ()=> {
          result = calculator.addTest(1,1);//colocamos los valores como parametros para la suma
          result.should.be.equal(2);//y seteamos el resultado que da la suma de los dos parametros, con el resultado real
       })
     })
   })
 
-  describe('Prueba con la funcion expect: ', function() {// este seria una forma mas VDD
-    describe('Comprovamos la funcion de addTest', function(){
-      it('Comprovamos el valor retornado usando: expect(result).to.be.a(value);: ', function() {
+  describe('Prueba con la funcion expect: ', ()=> {// este seria una forma mas VDD
+    describe('Comprovamos la funcion de addTest', ()=>{
+      it('Comprovamos el valor retornado usando: expect(result).to.be.a(value);: ', ()=> {
          result = calculator.addTest(1,1); //colocamos los valores como parametros para la suma
           expect(result).to.equal(3);//y seteamos el resultado que da la suma de los dos parametros, con el resultado real
       })
